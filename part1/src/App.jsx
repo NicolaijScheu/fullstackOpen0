a817a5d5732a713b384afe1bb0c8d9a0f95f45a0
NicolaijScheu <nicolaij.scheu@gmx.de>
2025-12-27 14:17:58 +0100

    add 3 components to render course title and number of excercises

---
+++ part1/src/App.jsx
@@ -0,0 +1,49 @@
+const Header = (props) => {
+  return (
+    <h1>{props.name}</h1>
+  )
+}
+
+const Content = (props) => {
+
+  return (
+    <>
+      <p>
+        {props.content[0].title} {props.content[0].excercises}
+      </p>
+      <p>
+        {props.content[1].title} {props.content[1].excercises}
+      </p>
+      <p>
+        {props.content[2].title} {props.content[2].excercises}
+      </p>
+    </>
+
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of excercises: {props.count}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header name={course} />
+      <Content content={[{ title: part1, excercises: exercises1 }, { title: part2, excercises: exercises2 }, { title: part3, excercises: exercises3 }]} />
+     <Total count={exercises1+exercises2+exercises3} />
+    </div>
+  )
+}
+
+export default App
